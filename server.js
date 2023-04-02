490d4fe1ae06de3670ec4fc31990e2645de70a76
rizwan933236 <114384405+rizwan933236@users.noreply.github.com>
2023-04-03 00:12:32 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,129 +10,133 @@ const SKey = "ThisisSecreatekey"
 
 const app = express()
 app.use(express.json())
-app.use(cors({origin:"*"}))
+app.use(cors({ origin: "*" }))
 
 //Middle for token 
-app.use("/excercise",(req,res,next)=>{
-    const token =req.headers.authorization
-    if(token==null){
-        res.status(401).send("Access denied")
-    }
-    else{
-        JWT.verify(token,SKey,(err,data)=>{
-            if(err){
-                res.status(401).send("Not verified")
-            }
-            else{
-                req.data = data
-                next()
-            }
+app.use("/excercise", (req, res, next) => {
+  const token = req.headers.authorization
+  if (token == null) {
+    res.status(401).send("Access denied")
+  }
+  else {
+    JWT.verify(token, SKey, (err, data) => {
+      if (err) {
+        res.status(401).send("Not verified")
+      }
+      else {
+        req.data = data
+        next()
+      }
 
-        })
+    })
 
-    }
+  }
 
 })
 
 //for User Resgistration 
 app.post("/register", async (req, res) => {
-    const { name, email, password } = req.body;
-    if(req.body == null){
+  const { name, email, password } = req.body;
+  if (req.body == null) {
     res.status(400).send("Invalid Credentials")
   }
   else
     try {
       const user = await User.findOne({ email });
-    
+
       if (user) {
         // User is already registered
         res.status(409).send({ message: "User already registered" });
       } else {
         // Create a new user
-         
+
         const result = await User.create({ name, email, password });
-       if(result == null){
-           res.status(400).send({
-          message: "Invalid credentials",
-        });
-       }else 
-        res.status(200).send({
-          message: "Register Successful",
-          User: result,
-        });
+        if (result == null) {
+          res.status(400).send({
+            message: "Invalid credentials",
+          });
+        } else
+          res.status(200).send({
+            message: "Register Successful",
+            User: result,
+          });
       }
     } catch (err) {
       res.status(500).send({ message: "Server error" });
     }
-  });
-  
+});
 
 //fro User Login
-app.post("/login",async (req,res)=>{
-    const {email,password} = req.body
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body
+  const result = await User.findOne({ email: email, password: password })
 
-    const result  = await User.findOne({email:email,password:password})
-
-    if(result==null){
-        res.status(400).send("Invalid Credentials")
-    }
-    else{
-        const Token = JWT.sign({id:result["_id"]},SKey)
-        res.status(200).send({Token:Token})
-    }
+  if (result == null) {
+    res.status(400).send("Invalid Credentials")
+  }
+  else {
+    const Token = JWT.sign({ id: result["_id"] }, SKey)
+    res.send({ Token: Token })
+  }
 })
 
 // for adding excercise 
-app.post("/excercise/addexcercise",async (req,res)=>{ 
-    const UserId  = req.data.id
-    const {title,des,type,duration} = req.body
-    try{
-        const result = await Excercise.create({title,des,type,duration})
-        await User.findByIdAndUpdate(UserId,{$push:{exercises: result["_id"]}})
+app.post("/excercise/addexcercise", async (req, res) => {
+  const UserId = req.data.id
+  const { title, des, type, duration } = req.body
+  try {
+    const result = await Excercise.create({ title, des, type, duration })
+    await User.findByIdAndUpdate(UserId, { $push: { exercises: result["_id"] } })
 
-        res.send({
-            status:(200),
-            message:"Excercise Added"
-        })
+    res.send({
+      status: (200),
+      message: "Excercise Added"
+    })
 
-    }
-    catch(err){
-        res.status(400).send({
-            message:"Excercise Not Added",
-            Error:err.message
-        })
-    }
+  }
+  catch (err) {
+    res.status(400).send({
+      message: "Excercise Not Added",
+      Error: err.message
+    })
+  }
 })
 
+//for Showing excercise data
+app.get("/excercise/getexcercise", async (req, res) => {
+  const UserId = req.data.id
+  const result = await User.findById(UserId).populate("exercises")
 
-//for get all excercise data 
-app.get("/excercise/getexcercise",async (req,res)=>{
-    const UserId = req.data.id
-    const result = await User.findById(UserId).populate("exercises")
-
-    res.status(200).send({exercises:result.exercises})
+  res.send({ exercises: result.exercises })
 })
 
-
-
-//for Get excercise data by id
-app.get("/excercise/getexcercise/:id",async (req,res)=>{
+//for Showing excercise data by id
+app.get("/excercise/getexcercise/:id", async (req, res) => {
   const exid = req.params.id
   const result = await Excercise.findById(exid)
-  res.send({exercise:result})
+  res.send({ exercise: result })
 })
 
+// for showing pie chart exercises 
+app.get('/pie/exercises', (req, res) => {
+  Excercise.aggregate([
+    { $group: { _id: '$type', totalDuration: { $sum: '$duration' }, count: { $sum: 1 } } }
+  ])
+    .then(exercises => res.json(exercises))
+    .catch(error => {
+      res.sendStatus(500);
+    });
+});
 
 //for editing existing excercise data
-app.put("/excercise/editExcercise/:id",async(req,res)=>{
- 
+app.put("/excercise/editExcercise/:id", async (req, res) => {
+
   try {
-    
     const exercise = await Excercise.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true,
     });
-   
+
     if (!exercise) {
       return res.status(404).json({ success: false, message: 'Exercise not found' });
     }
@@ -142,21 +146,19 @@ app.put("/excercise/editExcercise/:id",async(req,res)=>{
   }
 })
 
-
 //for deleting excwrcise data
-app.delete("/excercise/deleteExcercise/:id",async(req,res)=>{
-    try {
-       
-        const exercise = await Excercise.findByIdAndDelete(req.params.id);
-        if (!exercise) {
-          return res.status(404).json({ success: false, message: 'Exercise not found' });
-        }
-        res.json({ success: true, message: 'Exercise deleted successfully' });
-      } catch (err) {
-        res.status(500).json({ success: false, message: err.message });
-      }
+app.delete("/excercise/deleteExcercise/:id", async (req, res) => {
+  try {
+
+    const exercise = await Excercise.findByIdAndDelete(req.params.id);
+    if (!exercise) {
+      return res.status(404).json({ success: false, message: 'Exercise not found' });
+    }
+    res.json({ success: true, message: 'Exercise deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err.message });
+  }
 })
 
-// app.delete("/excercise/:exName",(req,res)=>{})
 
 app.listen(5000)
